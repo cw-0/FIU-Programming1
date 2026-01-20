a07456f1ebc5622e7b0ce15633bc31ea988a54f8
cw-0 <caden.warren47@gmail.com>
2026-01-20 11:26:31 -0500

    Lab 3a Done

--- COP2210_Lab3/src/app/Controller.java
+++ COP2210_Lab3/src/app/Controller.java
@@ -1,13 +1,77 @@
 package app;
+import java.lang.Math;
 
 public class Controller {
 
-    public static void main(String[] args){
+  public static void main(String[] args) {
+    yourInfoHeader("Lab 3");
+    displayHeaderForPartA();
+    double radius = 8.25;
+    double area = calculateAreaOfCircle(radius);
+    displaySingleResult(area, "calculateAreaOfCircle");
 
+    int value1 = 5;
+    double value2 = 59.5;
+    multipleNumbersIntAndDouble(value1, value2);
+  }
 
+  public static void yourInfoHeader(String assignment) {
 
-    };
+    System.out.println("==================================");
+    System.out.println("PROGRAMMER: "
+                       + "Caden Warren");
+    System.out.println("PANTHER ID: "
+                       + "6602666");
+    System.out.println();
+    System.out.println("CLASS: \t\t COP2210 ");
+    System.out.println("SECTION: \t "
+                       + "U01");
+    System.out.println("SEMESTER: \t "
+                       + "SPRING 2026");
+    System.out.println("CLASSTIME: \t "
+                       + "T/TH 9:30 - 12:15");
+    System.out.println();
+    System.out.println("Assignment: " + assignment);
+    System.out.println();
+    System.out.println(
+        "CERTIFICATION: \nI understand FIU’s academic policies, and I certify");
+    System.out.println("that this work is my own and that none of it is the");
+    System.out.println("work of any other person.");
+    System.out.println("==================================");
+    System.out.println();
+  }
 
+  public static void displayHeaderForPartA() {
+    System.out.println("============================");
+    System.out.println("SECTION Lab3a");
+    System.out.println("============================");
+    System.out.println();
+  }
 
+  public static double calculateAreaOfCircle(double radius) {
+    if (radius < 0) {
+      return -1;
+    }
+    return Math.PI * (radius * radius);
+  }
 
-}
+  public static void displaySingleResult(double value, String methodName) {
+    System.out.println("============================");
+    System.out.printf("%s\n", methodName);
+    System.out.println("============================");
+    System.out.printf("%-20s %.4f\n", "Value: ", value);
+    System.out.println();
+  }
+
+  public static void multipleNumbersIntAndDouble(int firstNumber,
+                                                 double secondNumber) {
+
+    double result = firstNumber * secondNumber;
+    System.out.printf("Running multipleNumbersIntAndDouble -> %d, %f\n",
+                      firstNumber, secondNumber);
+    System.out.printf("\tfirstNumber: \t\t%d\n", firstNumber);
+    System.out.printf("\tsecondNumber: \t\t%.1f\n", secondNumber);
+    System.out.printf("\tResult: \t\t%.1f\n", result);
+  }
+
+} // END CONTROLLER
