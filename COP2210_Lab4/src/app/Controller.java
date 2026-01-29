5781ddaec0e66421ee8c1775ad150a4f656ba37e
cw-0 <caden.warren47@gmail.com>
2026-01-29 12:11:19 -0500

    Complete Lab 4

--- COP2210_Lab4/src/app/Controller.java
+++ COP2210_Lab4/src/app/Controller.java
@@ -1,11 +1,77 @@
 package app;
 
+import vehicle.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller {
 
-    public static void main(String args[]){
+    public static void main(String[] args){
 
         yourInfoHeader("Lab 4");
 
+        List<Car> carList = new ArrayList<>();
+
+        Car.classDisplayInfo();
+        Car c1 = new Car();
+        carList.add(c1);
+
+        Car.classDisplayInfo();
+        Car c2 = new Car("White", 4);
+        carList.add(c2);
+
+        Car.classDisplayInfo();
+        Car c3 = new Car("Black", 2);
+        carList.add(c3);
+
+        Car.classDisplayInfo();
+        Car c4 = new Car("Blue", 4, 38500, 10432);
+        carList.add(c4);
+
+        Car.classDisplayInfo();
+
+        for (Car car:carList){
+            car.displayInfo();
+        }
+
+
+        System.out.println("------------------------------");
+        System.out.println("Working with instance method");
+        System.out.println("------------------------------");
+        c4.setColor("Green");
+        c4.setNumberOfDoors(2);
+        c4.displayInfo();
+
+
+        System.out.println("------------------------------");
+        System.out.println("Working with public instance variables");
+        System.out.println("------------------------------");
+        c2.displayInfo();
+        c2.doYouLikeIt = "No";
+        c2.displayInfo();
+
+
+        System.out.println("------------------------------");
+        System.out.println("Working with build-in string method");
+        System.out.println("------------------------------");
+        System.out.printf("%-39s %-20s\n", "Color of instance c1:", c1.getColor());
+        System.out.printf("%-39s %-20s\n", "Length of Color of instance c1:", (c1.getColor()).length());
+
+        System.out.println();
+
+        System.out.println("CarID of instance c2: \t\t\t\t\t" + c2.getCarId());
+        System.out.printf("Index of 'd' of c2's Car ID: \t\t\t%d\n", (c2.getCarId()).indexOf('d'));
+
+        System.out.println();
+
+        System.out.println("CarID of instance c2: \t\t\t\t\t" + c2.getCarId());
+        System.out.printf("Index of the first 'o' of c2's Car ID:\t%d\n", (c2.getCarId()).indexOf('o'));
+
+        System.out.println();
+
+        System.out.println("CarID of instance c2: \t\t\t\t\t" + c2.getCarId());
+        System.out.printf("Index of the second 'o' of c2's Car ID: %d\n", ((c2.getCarId()).replaceFirst("o", "~")).indexOf('o'));
     } // END OF MAIN
 
     public static void yourInfoHeader(String assignment) {
--- COP2210_Lab4/src/vehicle/Car.java
+++ COP2210_Lab4/src/vehicle/Car.java
@@ -1,103 +1,135 @@
 package vehicle;
 
-// TODO
-// Current Step: How to code the price and VIN number for each car:
 // underline, static
 // - , private
 // + , public
 // readOnly , const
 // : , type
 
+import java.util.Random;
+
 public class Car {
 
-    //-------------------
-    // class variables
-    //-------------------
+  //-------------------
+  // class variables
+  //-------------------
 
-    private static int vehicleCounter = 2000;
+  private static int vehicleCounter = 2000;
+  public static final Random randomGenerator = new Random();
+  //-------------------
+  // instance variables
+  //-------------------
 
-    //-------------------
-    // instance variables
-    //-------------------
+  private String carID;
+  private String color;
+  private String factory;
+  private int numberOfDoors;
+  private double price;
+  private int mileage;
+  public String doYouLikeIt;
 
-    private String carID;
-    private String color;
-    private String factory;
-    private int numberOfDoors;
-    private double price;
-    private int mileage;
-    public String doYouLikeIt;
+  //-------------------
+  // CONSTANTS
+  //-------------------
+  private static final String CAR_FACTORY_ID = "Norwood";
+  final double MIN_PRICE = 35000;
+  final double MAX_PRICE = 45000;
 
-    //-------------------
-    // CONSTANTS
-    //-------------------
-    private static final String CAR_FACTORY_ID = "Norwood";
 
-    //-------------------
-    // constructors
-    //-------------------
 
-    public Car(){
+  //-------------------
+  // constructors
+  //-------------------
 
-    }
+  public Car() {
+    color = "Red";
+    numberOfDoors = 2;
+    price = randomGenerator.nextDouble(MIN_PRICE, MAX_PRICE);
+    mileage = 0;
+    carID = "VIN" + '-' + CAR_FACTORY_ID + '-' + vehicleCounter;
+    factory = CAR_FACTORY_ID;
+    doYouLikeIt = "yes";
+    vehicleCounter++;
+  }
 
-    public Car(String color, int numberOfDoors){
+  public Car(String color, int numberOfDoors) {
+    this();
+    this.color = color;
+    this.numberOfDoors = numberOfDoors;
+  }
 
-    }
+  public Car(String color, int numberOfDoors, double price, int mileage) {
+    this();
+    this.color = color;
+    this.numberOfDoors = numberOfDoors;
+    this.price = price;
+    this.mileage = mileage;
+  }
 
-    public Car(String color, int numberOfDoors, double price, int mileage){
+  //-------------------
+  // getters & setters
+  //-------------------
+  public static int getVehicleCounter() {
+    return vehicleCounter;
+  }
 
-    }
+  public static void setVehicleCounter(int vehicleCounter) {
+    Car.vehicleCounter = vehicleCounter;
+  }
 
-    //-------------------
-    // getters & setters
-    //-------------------
-    public static int getVehicleCounter(){
+  public String getCarId() {
+    return carID;
+  }
 
-    }
+  public String getColor() {
+    return color;
+  }
 
-    public static void setVehicleCounter(int vehicleCounter){
+  public void setColor(String color) {
+    this.color = color;
+  }
 
-    }
+  public String getFactory() {
+    return CAR_FACTORY_ID;
+  }
 
-    public String getCarId(){
+  public int getNumberOfDoors() {
+    return numberOfDoors;
+  }
 
-    }
+  public void setNumberOfDoors(int numberOfDoors) {
+    this.numberOfDoors = numberOfDoors;
+  }
 
-    public String getColor(){
+  public double getPrice() {
+    return price;
+  }
 
-    }
+  //-------------------
+  //  class utils
+  //-------------------
 
-    public void setColor(String color){
+  public static void classDisplayInfo() {
+    System.out.println("------------------------------");
+    System.out.println("Car Class Info");
+    System.out.println("------------------------------");
+    System.out.printf("vehicleCounter: \t\t%s\n", Car.vehicleCounter);
+    System.out.printf("CAR_FACTORY_ID: \t\t%s\n", Car.CAR_FACTORY_ID);
+  }
 
-    }
+  // TODO FIX FORMAT
+  public void displayInfo() {
+    System.out.println("------------------------------");
+    System.out.println("Car Info");
+    System.out.println("------------------------------");
+    System.out.printf("%-25s %-15s\n", "CarID:", this.carID);
+    System.out.printf("%-25s %-15s\n", "Factory:", this.factory);
+    System.out.printf("%-25s %-15s\n", "Number of Doors:", this.numberOfDoors);
+    System.out.printf("%-25s %-15s\n", "Color:", this.color);
+    System.out.printf("%-25s %-15d\n", "Mileage:", this.mileage);
+    System.out.printf("%-25s $%-15.2f\n", "Price:", this.price);
+    System.out.printf("%-25s %-15s\n", "Do you like it:", this.doYouLikeIt);
 
-    public String getFactory(){
-
-    }
-
-    public int getNumberOfDoors(){
-
-    }
-
-    public void setNumberOfDoors(int numberOfDoors){
-
-    }
-
-    public double getPrice(){
-
-    }
-
-    //-------------------
-    //  class utils
-    //-------------------
-
-    public static void classDisplayInfor(){
-
-    }
-
-    public void displayInfo(){
-
-    }
+  }
 
 } // END OF CAR
